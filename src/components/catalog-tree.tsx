de7189272456deeb7593f3adfebd8b9e03826e8b
Jacob Chen <cwj9658@gmail.com>
2023-11-11 14:57:51 +0800

    complete catalog tree

--- src/components/catalog-tree.tsx
+++ src/components/catalog-tree.tsx
@@ -1,25 +1,43 @@
-/**
- * demo data structure
- * const mocTreeData: any[] = [
- *     {
- *         title: 'parent 0',
- *         key: '0-0',
- *         children: [
- *             {title: 'leaf 0-0', key: '0-0-0', isLeaf: true},
- *             {title: 'leaf 0-1', key: '0-0-1', isLeaf: true},
- *         ],
- *     },
- *     {
- *         title: 'parent 1',
- *         key: '0-1',
- *         children: [
- *             {title: 'leaf 1-0', key: '0-1-0', isLeaf: true},
- *             {title: 'leaf 1-1', key: '0-1-1', isLeaf: true},
- *         ],
- *     },
- * ];
- */
+import {DocumentTextIcon, FolderIcon} from "@heroicons/react/24/outline";
+import React from "react";
 
-export default function CatalogTree() {
+export interface TreeData {
+    title: string,
+    key: string,
+    children?: TreeData[],
+}
 
+export default function CatalogTree({treeData}: { treeData: TreeData[] }) {
+    const onItemClick: Function = (item: TreeData) => {
+        console.log(item);
+    }
+
+    return (
+        <div className="flex flex-col text-base my-2">
+            {renderTree({data: treeData, onItemClick})}
+        </div>
+    )
+
+}
+
+function renderTree({data, onItemClick}: { data: TreeData[], onItemClick: Function }): React.JSX.Element {
+
+    return (
+        <>
+            {data.map((item) => (
+                <div key={item.key}>
+                    <div className="flex flex-row p-1 cursor-pointer hover:bg-slate-300 rounded" key={item.key}
+                         onClick={() => onItemClick(item)}>
+                        {item.children?.length ?? 0
+                            ? <FolderIcon className="h-6 mr-2"/>
+                            : <DocumentTextIcon className="h-6 mr-2"/>}
+                        <span className="font-normal">{item.title}</span>
+                    </div>
+                    <div className="ml-3">
+                        {item.children && renderTree({data: item.children, onItemClick})}
+                    </div>
+                </div>
+            ))}
+        </>
+    )
 }
--- src/components/doc-side-nav.tsx
+++ src/components/doc-side-nav.tsx
@@ -1,11 +1,39 @@
-import {BarsArrowDownIcon, HomeIcon} from "@heroicons/react/24/outline";
+import {BarsArrowDownIcon, BookOpenIcon, HomeIcon} from "@heroicons/react/24/outline";
+import CatalogTree, {TreeData} from "./catalog-tree.tsx";
 
 export default function DocSideNav() {
+
+    const mocTreeData: TreeData[] = [
+        {
+            title: 'parent 0',
+            key: '0-0',
+            children: [
+                {
+                    title: 'parent 0-0', key: '0-0-0', children: [
+                        {title: 'leaf 0-0-0', key: '0-0-0-0'},
+                        {title: 'leaf 0-0-1', key: '0-0-0-1'},
+                    ]
+                },
+                {title: 'leaf 0-1', key: '0-0-1'},
+            ],
+        },
+        {
+            title: 'parent 1',
+            key: '0-1',
+            children: [
+                {title: 'leaf 1-0', key: '0-1-0'},
+                {title: 'leaf 1-1', key: '0-1-1'},
+            ],
+        },
+    ];
+
     return (
         <div className="mx-4 my-4 flex flex-col grow">
             <div className="h-12 mb-4 flex flex-row justify-start items-center">
-                <div className="h-7 w-7">log</div>
-                <div className="text-lg font-semibold">ZH doc space</div>
+                <div className="h-7 w-7 mr-2">
+                    <BookOpenIcon/>
+                </div>
+                <div className="text-lg font-semibold">ZH Doc's Space</div>
             </div>
             <div className="h-8 mb-4 flex flex-row justify-start items-center">
                 <HomeIcon className="h-6 mr-2"/>
@@ -16,9 +44,7 @@ export default function DocSideNav() {
                     <BarsArrowDownIcon className="h-6 mr-2"/>
                     <div className="text-base font-medium">Space Catalog</div>
                 </div>
-                <div>
-                    TreeComponent
-                </div>
+                <CatalogTree treeData={mocTreeData}/>
             </div>
         </div>
     )
